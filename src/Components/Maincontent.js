6d20ca8f1c7d102b3b2da2b4b5f654196204170b
Parth Parmar <parth3rdeye@gmail.com>
2020-04-30 20:05:20 +0530

    testing repo

--- src/Components/Maincontent.js
+++ src/Components/Maincontent.js
@@ -28,7 +28,7 @@ class Maincontent extends Component {
 
                 <div className="section-title">
                   <h2>About</h2>
-                  <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
+                  <p>My name is Parth. Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
                 </div>
 
                 <div className="row">
