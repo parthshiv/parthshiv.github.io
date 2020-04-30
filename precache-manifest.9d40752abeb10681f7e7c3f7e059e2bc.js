0eb9c2b0e8263fce93da6899a5947b9efa4b5b15
Parth Parmar <parth3rdeye@gmail.com>
2020-04-30 20:14:28 +0530

    Updates

---
+++ precache-manifest.9d40752abeb10681f7e7c3f7e059e2bc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b762a898e7fc04476175abf23522cc82",
+    "url": "https://parthshiv.github.io/index.html"
+  },
+  {
+    "revision": "5f353b10e578a733f2b9",
+    "url": "https://parthshiv.github.io/static/js/2.ff8bd651.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "https://parthshiv.github.io/static/js/2.ff8bd651.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0521486dba1efb849c34",
+    "url": "https://parthshiv.github.io/static/js/main.e01142d0.chunk.js"
+  },
+  {
+    "revision": "dbdb04397b2fc213ff14",
+    "url": "https://parthshiv.github.io/static/js/runtime-main.389d2a2e.js"
+  }
+]);
